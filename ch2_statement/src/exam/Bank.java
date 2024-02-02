fae5f4d111f558e25c5c7cf3f5028a5441c415f0
subinchoi <bbibbi7683@gmail.com>
2024-02-02 13:09:17 +0900

    반복문

--- ch2_statement/src/exam/Bank.java
+++ ch2_statement/src/exam/Bank.java
@@ -8,6 +8,9 @@ public class Bank {
     Scanner sc = new Scanner(System.in);
     boolean run = true;
 
+    int balance = 0;
+    // 잔액
+
     while (true) {
       System.out.println("-----------------");
       System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
@@ -17,6 +20,20 @@ public class Bank {
       int menu = sc.nextInt();
 
       switch (menu) {
+        case 1:
+          //예금액 입력 받은 후 잔액 추가
+          System.out.print("예금액 >>");
+          balance += sc.nextInt();
+          break;
+        case 2:
+          // 출금액 입력 받은 후 잔액 감소
+          System.out.print("출금액 >>");
+          balance -= sc.nextInt();
+          break;
+        case 3:
+          // 잔액 출력
+          System.out.println("잔액 >>" + balance);
+          break;
         case 4:
           System.out.println("프로그램 종료");
           run = false;
